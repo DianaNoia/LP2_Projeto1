34ce7cd22dbfc0580008e0a4f9c2221a62308079
Andrew <andrepedro62442@gmail.com>
2019-11-30 21:06:15 +0000

    Add FileAcess.cs

---
+++ Projeto1LP2/Assets/Scripts/FileAcess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileAcess : MonoBehaviour
+{
+
+    string folderPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        appName);
+
+    private const string appName = "MyIMDBSearcher";
+    private const string fileNameBasics = "name.basics.tsv.gz";
+    private const string fileTitleAkas = "title.akas.tsv.gz";
+    private const string fileTitleBasics = "title.basics.tsv.gz";
+    private const string fileTitleCrew = "title.crew.tsv.gz";
+    private const string fileTitleEpisode = "title.episode.tsv.gz";
+    private const string fileTitlePrincipals = "title.principals.tsv.gz";
+    private const string fileTitleRatings = "title.ratings.tsv.gz";
+}
+    
+    /* using (StreamReader sr = new StreamReader("TestFile.txt")) 
+            {
+                string line;
+                // Read and display lines from the file until the end of 
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null) 
+                {
+                    Debug.Log(line);
+                }
+            } 
+    */
+
+}
