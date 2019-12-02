fce105d9beb991dca3650b4157ef244d2ad77a94
Andrew <andrepedro62442@gmail.com>
2019-12-02 03:42:03 +0000

    edit FileAcess.cs and integration with unity

--- Projeto1LP2/Assets/Scripts/FileAcess.cs
+++ Projeto1LP2/Assets/Scripts/FileAcess.cs
@@ -8,18 +8,28 @@ using UnityEngine;
 public class FileAcess : MonoBehaviour
 {
 
-    private string appName = "MyIMDBSearcher";
+    private string appName = "MyIMDBSearcher/title.basics.tsv.gz";
 
     public void Decompress()
     {
         string folderWithFiles = Path.Combine(
-    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-    appName);
-        
-        GZipStream gz = new GZipStream(File.OpenRead() );
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        appName);
+
+        GZipStream gz = new GZipStream(File.OpenRead(folderWithFiles), CompressionMode.Decompress);
+
+        StreamReader sr = new StreamReader(gz);
+
+        using (sr)
+        {
+            while (sr.ReadLine() != null)
+            {
+                string line = sr.ReadLine();
+                Debug.Log(line);
+            }
+        }
 
 
     }
-
 
 }
