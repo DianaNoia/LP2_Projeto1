9bf65606f8015e6a8968e8b4fdad8b19838798d1
Andrew <andrepedro62442@gmail.com>
2019-12-02 02:40:18 +0000

    Start Decompress method in FileAcess.cs

--- Projeto1LP2/Assets/Scripts/FileAcess.cs
+++ Projeto1LP2/Assets/Scripts/FileAcess.cs
@@ -2,35 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using UnityEngine;
 
 public class FileAcess : MonoBehaviour
 {
 
-    string folderPath = Path.Combine(
-        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-        appName);
+    private string appName = "MyIMDBSearcher";
 
-    private const string appName = "MyIMDBSearcher";
-    private const string fileNameBasics = "name.basics.tsv.gz";
-    private const string fileTitleAkas = "title.akas.tsv.gz";
-    private const string fileTitleBasics = "title.basics.tsv.gz";
-    private const string fileTitleCrew = "title.crew.tsv.gz";
-    private const string fileTitleEpisode = "title.episode.tsv.gz";
-    private const string fileTitlePrincipals = "title.principals.tsv.gz";
-    private const string fileTitleRatings = "title.ratings.tsv.gz";
-}
-    
-    /* using (StreamReader sr = new StreamReader("TestFile.txt")) 
-            {
-                string line;
-                // Read and display lines from the file until the end of 
-                // the file is reached.
-                while ((line = sr.ReadLine()) != null) 
-                {
-                    Debug.Log(line);
-                }
-            } 
-    */
+    public void Decompress()
+    {
+        string folderWithFiles = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+    appName);
+        
+        GZipStream gz = new GZipStream(File.OpenRead() );
+
+
+    }
+
 
 }
