36bf055aca97a6102c5a48ddc637e2b869d50c78
ineesgoncalves <ines98goncalves@gmail.com>
2019-12-16 10:26:10 +0000

    Add methods to make the menu

--- Projeto1_LP2/MenuUI.cs
+++ Projeto1_LP2/MenuUI.cs
@@ -10,7 +10,7 @@ namespace Projeto1_LP2
 {
     class MenuUI
     {
-        //SearchFiles sf = new SearchFiles();
+        private static SearchFiles sf = new SearchFiles();
         private const int numTitlesToShowOnScreen = 10;
 
         public void ShowMemory()
@@ -29,21 +29,22 @@ namespace Projeto1_LP2
             Console.WriteLine();
         }
 
-        public void ShowSearchResults(Title[] queryResults)
+        public void ShowSearchResults(Title[] queryResults, string searchText)
         {
             int numTitlesShown = 0;
 
-            //Console.WriteLine("Insert search term");
-            //searchText = Console.ReadLine();
-            //sf.FileSearch(searchText);
+            Console.WriteLine("Insert search term");
+
+            Console.WriteLine($"\t=> there are {queryResults.Count()} titles"
+                + $"with {searchText}");
 
             while (numTitlesShown < queryResults.Length)
             {
                 Console.WriteLine($"\t => Press key to see next " +
                     $"{numTitlesToShowOnScreen} titles");
 
-                for (int i = numTitlesShown; 
-                    i < numTitlesShown + numTitlesToShowOnScreen && 
+                for (int i = numTitlesShown;
+                    i < numTitlesShown + numTitlesToShowOnScreen &&
                     i < queryResults.Length; i++)
                 {
                     bool firstGenre = true;
@@ -55,7 +56,7 @@ namespace Projeto1_LP2
                     Console.Write(
                         $"({title.StartYear?.ToString() ?? "unknown year"}): ");
 
-                    foreach(string genre in title.Genres)
+                    foreach (string genre in title.Genres)
                     {
                         if (!firstGenre) Console.Write("/");
                         Console.Write($"{genre} ");
@@ -71,5 +72,75 @@ namespace Projeto1_LP2
             }
         }
 
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. Search title");
+            Console.WriteLine("2. Credits");
+            Console.WriteLine("3. Quit");
+            Console.WriteLine();
+            Console.WriteLine("Write the correspondent number to proceed\n");
+
+            ReadEntry();
+        }
+
+        public void ShowCredits()
+        {
+            Console.WriteLine("André Pedro      a21701115");
+            Console.WriteLine("Diana Nóia       a21703004");
+            Console.WriteLine("Inês Gonçalves   a21702076");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to go back to the main menu\n");
+            Console.ReadKey();
+            Console.Clear();
+            ShowMenu();
+        }
+
+        public void ReadEntry()
+        {
+            // A loop that will run while the condition is true, and while the
+            // player doesn't choose to leave
+            while (true)
+            {
+                // A switch case that will work based on the player's input
+                switch (Console.ReadLine())
+                {
+                    // If the player chooses 1
+                    case "1":
+                        Console.Clear();
+                        string inputSupport;
+                        Title[] queryResults = sf.FileSearch(out inputSupport);
+                        ShowSearchResults(queryResults, inputSupport);
+                        break; ;
+
+                    // If the player chooses 3
+                    case "2":
+                        Console.Clear();
+                        ShowCredits();
+                        break;
+
+                    // If the player chooses 4
+                    case "3":
+                        Console.Clear();
+                        // Will break the loop and exit the program
+                        Environment.Exit(0);
+                        break;
+
+                    // A default message in case the player chooses an
+                    // unavailable number
+                    default:
+                        // Will print an error message, and ask them to type a
+                        // number again
+                        Console.WriteLine("Not a valid number. Try again.");
+                        // Reads the input
+                        Console.ReadLine();
+                        // Will clear the console
+                        Console.Clear();
+                        // Will call the menu
+                        ShowMenu();
+                        break;
+                }
+            }
+
+        }
     }
 }
