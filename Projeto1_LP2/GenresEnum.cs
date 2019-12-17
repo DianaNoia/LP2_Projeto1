b84356c75979b8a693efd0c25cfff210644311c9
ineesgoncalves <ines98goncalves@gmail.com>
2019-12-17 11:41:01 +0000

    Created new enum with all genres, so the method ShowGenres could work without seachershing on files first

---
+++ Projeto1_LP2/GenresEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto1_LP2
+{
+    public enum GenresEnum
+    {
+        Action,
+        Adult,
+        Adventure,
+        Animation,
+        Biography,
+        Comedy,
+        Crime,
+        Documentary,
+        Drama,
+        Family,
+        Fanatsy,
+        FilmNoir,
+        GameShow,
+        History,
+        Horror,
+        Music,
+        Musical,
+        Mystery,
+        News,
+        RealityTV,
+        Romance,
+        SciFi,
+        Short,
+        Sport,
+        TalkShow,
+        Thriller,
+        War,
+        Western
+    }
+}
